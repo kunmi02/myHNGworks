f4a2a0733ae1c956c7f668f1355ab88a4a448cf9
Azeez Akinkunmi <mrkhunmie@gmail.com>
2018-04-24 17:51:09 +0200

    Add files via upload

---
+++ HNG_stage1/hng.php
@@ -0,0 +1,33 @@
+
+
+<html>
+<head>
+<title>HNG Internship</title>
+<link rel="stylesheet" type="text/css" href="hngcss.css">
+</head>
+<body class="bg">
+<div align="right"> 
+<nav>
+<img src="home.png" ><img src="about.png"><img src="join.png"><img src="contact.png">
+</nav>
+
+</div>
+<div class="midtext">
+<p><h1><strong>HNG INTERNSHIP 4</h1></font><br/></p>
+<p> <?php
+echo "<strong> ";
+echo "<font color='green'>";
+echo "<h1>";
+date_default_timezone_set("Africa/Lagos");
+echo "The time is " . date("h:i:sa"). "<br>";
+echo "The date is is " . date("d/m/Y") . "<br>";
+echo "<h1>";
+echo "</strong> ";
+echo "</font>";
+?></p>
+</div>
+
+
+</body>
+</html>
+
